ebd3e01b689db6b3e3c9caa1d297f97eecc57249
Olivier BYIRINGIRO <yuppiegvng@gmail.com>
2023-09-08 16:14:02 +0200

    Initialised some variables and handled the display of dashes

--- hangman_game.py
+++ hangman_game.py
@@ -2,3 +2,21 @@
 # KATUMBA Frank
 # BYIRINGIRO Olivier
 # MUNEZERO Michel
+
+import random
+
+word_list = ["apple", "dog", "cat", "tree", "house", "book", "sun", "water", "friend", "smile"]
+random_word = random.choice(word_list).lower()  # Convert random_word to lowercase
+lives = 6
+display = []
+
+game_over = False
+#print(random_word)
+
+for letter in random_word:
+    display += "_"
+
+print(' '.join(display))  # Join the dashes and display as a single string
+
+guessed_letters = []
+warnings = 3
